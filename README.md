a0641bc248c41ee9060303bff491072712f93da2
Terry <2358269014@qq.com>
2018-01-30 15:29:46 +0800

    修改config

--- README.md
+++ README.md
@@ -22,6 +22,15 @@ go get github.com/fecshop/go_fec_api
 ```
 
 
+配置
+----------
+
+进入文件夹：`github.com/fecshop/go_fec_api/config/` 
+
+新建文件 `config.go` ，将 `config_example.go` 的内容复制进去，
+将1,2行 和最后一行的注释部分去掉
+
+然后，设置里面相应的参数即可
 
 
 
--- config/config_example.go
+++ config/config_example.go
@@ -9,19 +9,19 @@ import (
 
 func Get(key string) string{
     cf := make(map[string]string)
-    cf["logFileDir"]    = `/www/web_logs/go_fec_api/`
-    cf["logGlobalFile"] = `global.log`
-    cf["server_ip"]     = `120.24.37.249`
-    cf["server_port"]   = `3000`
-    cf["token"]   = `xxxx`
-    
-    cf["mysql_host"]   = `127.0.0.1:3306`
-    cf["mysql_database"]   = `fecshop`
-    cf["mysql_user"]   = `root`
-    cf["mysql_password"]   = `xxxx`
-    cf["mysql_charset"]   = `utf8`
-    cf["mysql_maxOpenConns"]   = 200
-    cf["mysql_maxIdleConns"]   = 100
+    cf["logFileDir"]    = `/www/web_logs/go_fec_api/`   // log存放的地址
+    cf["logGlobalFile"] = `global.log`                  // log文件
+    cf["server_ip"]     = `120.24.37.249`               // http服务对应的ip
+    cf["server_port"]   = `3000`                        // http服务对应的prot
+    cf["token"]         = `xxxxx` // 验证token，这个和fecshop_enterprise 里面的token值要对应，这个相当于apiKey，因为是内网访问，因此做一个key值验证就行了。
+    // mysql配置
+    cf["mysql_host"]    = `127.0.0.1:3306`  // mysql host
+    cf["mysql_user"]    = `root`            // mysql user
+    cf["mysql_database"]         = `fecshop`
+    cf["mysql_password"]         = `xxxx`
+    cf["mysql_charset"]          = `utf8`
+    cf["mysql_maxOpenConns"]     = `200`    // mysql 用于设置最大打开的连接数，默认值为0表示不限制。 
+    cf["mysql_maxIdleConns"]     = `100`    // mysql 用于设置闲置的连接数。
     
     return cf[key]
 }
